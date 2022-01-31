59cb388aeab73eec09cb0ea87bf259d901e16fd2
Romana Rashid <romanacse98@gmail.com>
2022-02-01 01:12:36 +0600

    first commit

---
+++ for-loop.js
@@ -0,0 +1,6 @@
+// for (var roastGiven = 0; roastGiven < 7; roastGiven++) {
+//     console.log(roastGiven);
+// }
+ for (i = 0; i <= 10; i = i+2){
+        console.log(i);
+ }
---
+++ while-loop-example.js
@@ -0,0 +1,14 @@
+var number = 0; //loop variable
+// while(number <= 10){
+//     console.log(number);
+//     number++;
+// }
+//odd num
+// while(number <= 20){
+//     console.log(number);
+//     number = number + 2;
+// }
+while(number <= 20){
+    console.log(number);
+    number = number + 2;
+}
